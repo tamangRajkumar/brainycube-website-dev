ad14c0b3ff8004d609747129b004748b4a766d1e
subinsatyal <satyalsubin33@gmail.com>
2023-03-14 13:19:40 +0545

    nav and about us added

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
+import Navbar from "@/src/components/HeaderFooter/Nav";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />;
+    </>
+  );
 }
--- public/index.js
+++ public/index.js
@@ -1,1 +1,1 @@
-export { default as destinationCover } from "./assets/images/destinationCover.jpg";
+export {default as Logo} from "./assets/images/logo.jpg";
